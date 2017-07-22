da74b1673281acbaddc25f7ebac307f668378e47
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-07-22 23:21:14 +0530

    added session, break plus minus buttons are working

--- script/script.js
+++ script/script.js
@@ -1,6 +1,52 @@
 $(document).ready(function(){
-	
-	var clock = $('.your-clock').FlipClock({
-	// ... your options here
+	let countS = parseInt($('.sessionTime').text());
+	let countB = parseInt($('.breakTime').text());
+	let stateSession = $('h2').text();
+	console.log(stateSession);
+	let count;
+	var clock = $('.my-clock').FlipClock(0,{
+		countdown:true,
+		clockFace: 'MinuteCounter',
+		autoStart:false,
+		callbacks:{
+			interval:function(){
+				if(clock.getTime() == 0){
+					if(stateSession == 'session'){	
+						clock.setTime(countB);
+						stateSession == 'break';
+						console.log(stateSession);
+						$('h2').text(stateSession);
+					}else {
+						clock.setTime(countS);
+						stateSession = 'session';
+						$('h2').text(stateSession);
+					}
+				}
+			}
+		}
+	});
+	$('.clock_start').on('click',function(){
+			if (countS != 0 && this.stateSession !== 'session') {
+				stateSession = 'session';
+				// clock.setTime(countS);
+				clock.start();
+				$('h2').text(stateSession);
+			}
+	});
+	$('.sessionInc').on('click',function(){
+		countS += 1;
+		$('.sessionTime').text(countS);
+	});
+	$('.sessionDec').on('click',function(){
+		countS -= 1;
+		$('.sessionTime').text(countS);
+	});
+	$('.breakInc').on('click',function(){
+		countB += 1;
+		$('.breakTime').text(countB);
+	});
+	$('.breakDec').on('click',function(){
+		countB -= 1;
+		$('.breakTime').text(countB);
 	});
 });
