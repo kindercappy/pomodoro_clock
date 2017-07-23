97e481d3f1402906d16bc58fcc4a72c60edc8c12
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-07-23 21:32:13 +0530

    clock working, added audio, stop, clear and pause buttons

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
-	let countS = parseInt($('.sessionTime').text());
-	let countB = parseInt($('.breakTime').text());
-	let stateSession = $('h2').text();
-	console.log(stateSession);
+	// https://notificationsounds.com/soundfiles/35051070e572e47d2c26c241ab88307f/file-74_bells-message.mp3
+	let countS = 12;
+	let countB = 11;
+	$('.sessionTime').text(countS);
+	$('.breakTime').text(countB);
+	let stateSession = undefined;
+	let sessionString = "Session Started";
+	let breakString = "Break time";
+	let paused = "Paused";
+	let clear = 'Pomodoro Clock';
+	var posLama;
+	var countLama;
 	let count;
 	var clock = $('.my-clock').FlipClock(0,{
 		countdown:true,
@@ -10,29 +18,26 @@ $(document).ready(function(){
 		autoStart:false,
 		callbacks:{
 			interval:function(){
-				if(clock.getTime() == 0){
-					if(stateSession == 'session'){	
+				var time = clock.getTime().time;
+            	if (time === 0) {
+            		$('audio').trigger("play");
+            	}
+				if(clock.getTime().time == 0){
+					if(stateSession){	
 						clock.setTime(countB);
-						stateSession == 'break';
-						console.log(stateSession);
-						$('h2').text(stateSession);
-					}else {
+						clock.start();
+						stateSession = false;
+						$('h2').text(breakString);
+					}else if(!stateSession){
 						clock.setTime(countS);
-						stateSession = 'session';
-						$('h2').text(stateSession);
+						clock.start();
+						stateSession = true;
+						$('h2').text(sessionString);
 					}
 				}
 			}
 		}
-	});
-	$('.clock_start').on('click',function(){
-			if (countS != 0 && this.stateSession !== 'session') {
-				stateSession = 'session';
-				// clock.setTime(countS);
-				clock.start();
-				$('h2').text(stateSession);
-			}
-	});
+	})
 	$('.sessionInc').on('click',function(){
 		countS += 1;
 		$('.sessionTime').text(countS);
@@ -48,5 +53,36 @@ $(document).ready(function(){
 	$('.breakDec').on('click',function(){
 		countB -= 1;
 		$('.breakTime').text(countB);
+	});
+	$('.clock_start').on('click',function(){
+			if (stateSession === undefined || clock.getTime() === 0) {
+				clock.setTime(countS);
+				stateSession = true;
+			$('h2').text(sessionString);
+			}else{
+				if (stateSession) {
+					$('h2').text(sessionString);
+				}else if(!stateSession){
+					$('h2').text(breakString);
+				}
+			}
+			clock.start();
+	});
+	$('.clock_pause').on('click',function(){
+		clock.stop();
+		if(stateSession){
+			$('h2').text("Session " + paused);
+		}else if(!stateSession){
+			$('h2').text("Break " + paused);
+		}
+	});
+	$('.clock_clear').on('click',function(){
+		if(stateSession !== undefined){
+			stateSession = undefined;
+			clock.stop();
+			clock.setTime(0);
+			
+			$('h2').text(clear);
+		}
 	});
 });
