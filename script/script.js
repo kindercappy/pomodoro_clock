183ee7e72b338e01154d3b008b90f6d9091d1034
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-07-24 13:11:01 +0530

    app completed. uploaded on codepen

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-	// https://notificationsounds.com/soundfiles/35051070e572e47d2c26c241ab88307f/file-74_bells-message.mp3
-	let countS = 12;
-	let countB = 11;
+	let countS = 25;
+	let countB = 5;
 	$('.sessionTime').text(countS);
 	$('.breakTime').text(countB);
 	let stateSession = undefined;
@@ -9,9 +8,6 @@ $(document).ready(function(){
 	let breakString = "Break time";
 	let paused = "Paused";
 	let clear = 'Pomodoro Clock';
-	var posLama;
-	var countLama;
-	let count;
 	var clock = $('.my-clock').FlipClock(0,{
 		countdown:true,
 		clockFace: 'MinuteCounter',
@@ -24,12 +20,12 @@ $(document).ready(function(){
             	}
 				if(clock.getTime().time == 0){
 					if(stateSession){	
-						clock.setTime(countB);
+						clock.setTime(countB*60);
 						clock.start();
 						stateSession = false;
 						$('h2').text(breakString);
 					}else if(!stateSession){
-						clock.setTime(countS);
+						clock.setTime(countS*60);
 						clock.start();
 						stateSession = true;
 						$('h2').text(sessionString);
@@ -39,24 +35,32 @@ $(document).ready(function(){
 		}
 	})
 	$('.sessionInc').on('click',function(){
-		countS += 1;
+		if(countS > 0 ){
+			countS += 1;
+		}
 		$('.sessionTime').text(countS);
 	});
 	$('.sessionDec').on('click',function(){
-		countS -= 1;
+		if(countS > 1 ){
+			countS -= 1;
+		}
 		$('.sessionTime').text(countS);
 	});
 	$('.breakInc').on('click',function(){
-		countB += 1;
+		if(countB > 0 ){
+			countB += 1;
+		}
 		$('.breakTime').text(countB);
 	});
 	$('.breakDec').on('click',function(){
-		countB -= 1;
+		if(countB > 1 ){
+			countB -= 1;
+		}
 		$('.breakTime').text(countB);
 	});
 	$('.clock_start').on('click',function(){
 			if (stateSession === undefined || clock.getTime() === 0) {
-				clock.setTime(countS);
+				clock.setTime(countS*60);
 				stateSession = true;
 			$('h2').text(sessionString);
 			}else{
@@ -72,6 +76,8 @@ $(document).ready(function(){
 		clock.stop();
 		if(stateSession){
 			$('h2').text("Session " + paused);
+		}else if (stateSession === undefined){
+			$('h2').text(clear);
 		}else if(!stateSession){
 			$('h2').text("Break " + paused);
 		}
@@ -81,7 +87,7 @@ $(document).ready(function(){
 			stateSession = undefined;
 			clock.stop();
 			clock.setTime(0);
-			
+
 			$('h2').text(clear);
 		}
 	});
